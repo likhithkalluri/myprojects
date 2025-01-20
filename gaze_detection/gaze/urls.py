2913a04f79199716fb0ff2e26168deee5fe20605
LIKHITH KUMAR <likhithkumar@spotmies.ai>
2025-01-20 10:15:00 +0530

    Delete gaze_detection directory

--- gaze_detection/gaze/urls.py
+++
@@ -1,9 +0,0 @@
-# gaze/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Home page
-    path('detect_gaze/', views.detect_gaze, name='detect_gaze'),  # Gaze detection endpoint
-    path('stream_video/', views.stream_video, name='stream_video'),  # Video feed endpoint
-]
